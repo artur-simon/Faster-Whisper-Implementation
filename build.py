30f5ba43553770cbb5dacda49732b639dea5182f
Artur Simon <artur_simon@hotmail.com>
2025-10-17 04:38:48 -0300

    (feat,chore) add build script, add icon, update requirements

---
+++ build.py
@@ -0,0 +1,13 @@
+import os, PyInstaller.__main__, site
+
+site_packages = site.getsitepackages()[1]
+assets_path = os.path.join(site_packages, "faster_whisper", "assets")
+
+PyInstaller.__main__.run([
+    'app.py',
+    '--onefile',
+    '--icon=wisp.ico',
+    '--add-data=wisp.ico;.',
+    f'--add-data={assets_path};faster_whisper/assets',
+    '--name=WispLive'
+])
